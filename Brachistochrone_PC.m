c6864ed144321a661c3048b734675d0bd706680e
mmm3-k <64644159+mmm3-k@users.noreply.github.com>
2026-06-29 19:19:35 +0530

    Update Brachistochrone_PC.m
    Errors_corrected.

--- Brachistochrone_PC.m
+++ Brachistochrone_PC.m
@@ -21,13 +21,13 @@ dt = MX.sym('dt');
 
 %RK4
 k1 = dynamics(x,u);
-k2 = dynamics(x + (DT/2)*k1, u);
-k3 = dynamics(x + (DT/2)*k2, u);
-k4 = dynamics(x + DT*k3, u);
-x_next = x + (DT/6)*(k1 + 2*k2 + 2*k3 + k4);
+k2 = dynamics(x + (dt/2)*k1, u);
+k3 = dynamics(x + (dt/2)*k2, u);
+k4 = dynamics(x + dt*k3, u);
+x_next = x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4);
 
 %Symbolic Blue print
-F = Function('F', {x,u,DT},{x_next});
+F = Function('F', {x,u,dt},{x_next});
 
 U = MX.sym('U',N);
 U0 = 0.1* ones(N,1);
@@ -62,7 +62,7 @@ ubu = pi * ones(N,1);
 
 %Step size constraints
 lb_dt = 0.001;
-up_dt = 1.0;
+ub_dt = 1.0;
 
 %Combine two constraints
 lbw = [lbu;lb_dt];
@@ -96,13 +96,13 @@ plot(x_line,y_line, '--r', 'LineWidth', 1.5);
 grid on;
 title('Brachistochrone Path Profile');
 xlabel('Horizontal Position x (m)');
-ylabel('Vertical Position y (m););
+ylabel('Vertical Position y (m)');
 legend('Optimal Trajectory','Path Boundary Constraint(y \leq x/2 + h)','Location', 'best');
 
 %Control Profile
 subplot(2,1,2);
 stairs(time_steps, [U_opt;nan],'m','LineWidth',1.5);
 grid on;
-title('Optimal control angle trajectory u(t));
+title('Optimal control angle trajectory u(t)');
 xlabel('Time (sec)');
 ylabel('Angle u (radians)');
